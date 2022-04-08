0bd66276dccdf7f7184bae40eb2831121bdc16c5
upparshilpa <102309138+upparshilpa@users.noreply.github.com>
2022-04-08 13:11:12 +0530

    Add files via upload

---
+++ ReactProgrammingClasscomponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Functionone extends Component{
+    render(){
+        return(
+            <div>
+                <h1>class component</h1>
+                <p>This is the 1st code in the class components react</p>
+            </div>
+        )
+    }
+}
+export default Functionone
---
+++ ReactprogrammingFunctionalComponent1.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+function Sample()
+{
+    return(
+        <div>
+            <p>This is the 1st practice of function</p>
+        </div>
+    )
+}
+export default Sample
